bce1f0a20dddaefa69af18f48b57ea70e0ee769c
sambender97 <sambender97@gmail.com>
2016-03-14 15:48:51 -0400

    updated charts

---
+++ src/com/design/charts/QueryClassPieChart.java
@@ -0,0 +1,95 @@
+package com.design.charts;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import com.design.persistence.Queries;
+import com.design.persistence.Users;
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.Cursor;
+import com.vaadin.addon.charts.model.DataLabels;
+import com.vaadin.addon.charts.model.DataSeries;
+import com.vaadin.addon.charts.model.DataSeriesItem;
+import com.vaadin.addon.charts.model.PlotOptionsPie;
+import com.vaadin.addon.charts.model.Tooltip;
+import com.vaadin.addon.jpacontainer.JPAContainer;
+import com.vaadin.addon.jpacontainer.JPAContainerFactory;
+import com.vaadin.data.Container.Filter;
+import com.vaadin.data.util.filter.Compare;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+public class QueryClassPieChart extends VerticalLayout {
+
+	Chart chart;
+	Configuration conf;
+	DataSeries series;
+	
+	static EntityManager em = Persistence.createEntityManagerFactory("DesignProject").createEntityManager();
+	
+	public QueryClassPieChart () {
+		ui();
+	}
+	public void ui() {
+		
+		
+		chart = new Chart(ChartType.PIE);
+
+		conf = chart.getConfiguration();
+
+		conf.setTitle("Queries by Format");
+
+		Tooltip tooltip = conf.getTooltip();
+		tooltip.setPointFormat("<b>{point.percentage:.1f}%</b>");
+
+		PlotOptionsPie plotOptions = new PlotOptionsPie();
+		plotOptions.setAllowPointSelect(true);
+		plotOptions.setCursor(Cursor.POINTER);
+		DataLabels dataLabels = plotOptions.getDataLabels();
+		dataLabels.setEnabled(true);
+		dataLabels.setFormat("{point.name}");
+		conf.setPlotOptions(plotOptions);
+
+		series = new DataSeries("Revenue");
+		series.add(new DataSeriesItem("Math", 25));
+		series.add(new DataSeriesItem("Weather", 25));
+		series.add(new DataSeriesItem("News", 25));
+		series.add(new DataSeriesItem("Directions", 25));
+		conf.setSeries(series);
+		
+		
+		chart.setSizeFull();
+		this.addComponent(chart);
+		
+		
+		
+		loadData();
+		
+	}
+	public void loadData () {
+		
+		String query = "SELECT x from Queries AS x";
+		
+		List <Queries> queries = em.createQuery(query).getResultList();
+		int math = queries.stream().filter(p -> p.getClass1().equals("math")).collect(Collectors.toList()).size();
+		int weather = queries.stream().filter(p -> p.getClass1().equals("weather")).collect(Collectors.toList()).size();
+		int directions = queries.stream().filter(p -> p.getClass1().equals("directions")).collect(Collectors.toList()).size();
+		int news = queries.stream().filter(p -> p.getClass1().equals("news")).collect(Collectors.toList()).size();
+
+		
+		series.get("Math").setY(math);
+		series.get("Weather").setY(weather);
+		series.get("News").setY(news);
+		series.get("Directions").setY(directions);
+		
+	}
+}
--- src/com/design/communicate/VoiceProcessing.java
+++ src/com/design/communicate/VoiceProcessing.java
@@ -7,14 +7,17 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 
 public class VoiceProcessing {
+	
+	public String getString (File audio) {
+		SpeechToText service = new SpeechToText();
+		service.setUsernameAndPassword("6ce30912-e2be-4a99-9e7a-4e712c5d0cd5", "6XZo4JOLKy6W");
 
-	SpeechToText service = new SpeechToText();
-	service.setUsernameAndPassword("6ce30912-e2be-4a99-9e7a-4e712c5d0cd5", "6XZo4JOLKy6W");
 
-	File audio = new File("src/test/resources/sample1.wav");
+		SpeechResults transcript = service.recognize(audio, HttpMediaType.AUDIO_WAV);
+		return transcript.toString();
+	}
 
-	SpeechResults transcript = service.recognize(audio, HttpMediaType.AUDIO_WAV);
-	System.out.println(transcript);
+	
 	
 	
 }
