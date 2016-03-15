9e4dffebfa4d73faf4113571fc987a99419e76a7
ShaynAli <shayaan.syed.ali@gmail.com>
2016-03-14 21:19:53 -0400

    Deleted old voice files

---
+++ src/com/design/servlets/TwilioServlet.java
@@ -0,0 +1,37 @@
+package com.design.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.twilio.sdk.verbs.Say;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
+
+@WebServlet("/echo")
+public class TwilioServlet extends HttpServlet
+{
+
+	public void service(HttpServletRequest HttpRequest, HttpServletResponse HttpResponse) throws IOException
+	{
+		TwiMLResponse response = new TwiMLResponse();
+		Say say = new Say("Hello world");
+		
+		try
+		{
+			response.append(say);
+		}
+		catch(TwiMLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		HttpResponse.setContentType("applicaiton/xml");
+		HttpResponse.getWriter().print(response.toXML());
+		
+	} // service class
+	
+} // TwilioServlet class
---
+++ src/com/design/servlets/TwilioVoiceServlet.java
@@ -0,0 +1,10 @@
+package com.design.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/voice")
+public class TwilioVoiceServlet extends HttpServlet
+{
+	
+} // TwilioVoiceServlet class
