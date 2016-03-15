eef59bed5ea44e2b13d4ffa20fdf35a19fa9678e
ShaynAli <shayaan.syed.ali@gmail.com>
2016-03-14 22:54:46 -0400

    Echo test setup

--- src/com/design/servlets/TwilioServlet.java
+++ src/com/design/servlets/TwilioServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.twilio.sdk.verbs.Record;
+import com.twilio.sdk.verbs.Say;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.TwiMLResponse;
 @WebServlet("/echo")
@@ -15,17 +16,18 @@ public class TwilioServlet extends HttpServlet
 	public void service(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException
 	{
 		TwiMLResponse response = new TwiMLResponse();
-//		Say say = new Say("VOICE VOICE VOICE VOICE VOICE VOICE VOICE VOICE");
 		
-		String callerNumber = httpRequest.getParameter("From");
+//		String callerNumber = httpRequest.getParameter("From");
 		
 		Record rec = new Record();
 		rec.setMaxLength(30);
 		
+		rec.setAction("/voice-servlet");
 		// Send a response
 		try
 		{
-//			response.append(say);
+			// Prompt
+			response.append(new Say("Please say your query in a clear tone"));
 			response.append(rec);
 		}
 		catch(TwiMLException e)
--- src/com/design/servlets/TwilioVoiceServlet.java
+++ src/com/design/servlets/TwilioVoiceServlet.java
@@ -1,10 +1,48 @@
 package com.design.servlets;
 
+import java.io.IOException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.twilio.sdk.verbs.Play;
+import com.twilio.sdk.verbs.Say;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
 
 @WebServlet("/voice-servlet")
 public class TwilioVoiceServlet extends HttpServlet
 {
+	
+	public void service(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException
+	{
+		String recordingUrl = httpRequest.getParameter("RecordingURL");
+		TwiMLResponse response = new TwiMLResponse();
+		
+		if (recordingUrl != null)
+		{
+			try
+			{
+				// Give user heads up on playing recording
+				response.append(new Say ("Here is what was picked up"));
+				response.append(new Play (recordingUrl));
+				response.append(new Say ("End of message"));
+			}
+			catch(TwiMLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			httpResponse.sendRedirect("/echo");
+			return;
+		}
+		
+		httpResponse.setContentType("application/xml");
+		httpResponse.getWriter().print(response.toXML());
+	}
 	
 } // TwilioVoiceServlet class
