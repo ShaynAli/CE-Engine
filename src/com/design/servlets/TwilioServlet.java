802c41559b53cd3de78719bbe8abdcecf4f2168e
ShaynAli <shayaan.syed.ali@gmail.com>
2016-03-14 21:37:06 -0400

    Application spelling fixed

--- src/com/design/servlets/TwilioServlet.java
+++ src/com/design/servlets/TwilioServlet.java
@@ -29,7 +29,7 @@ public class TwilioServlet extends HttpServlet
 			e.printStackTrace();
 		}
 		
-		HttpResponse.setContentType("applicaiton/xml");
+		HttpResponse.setContentType("applicition/xml");
 		HttpResponse.getWriter().print(response.toXML());
 		
 	} // service class
