5ec0136691538f24cf6c8cefa59b15ad7006988c
ShaynAli <shayaan.syed.ali@gmail.com>
2016-03-15 01:22:44 -0400

    VoiceProcessing stream file form URL to convert audio to text

--- src/com/design/communicate/VoiceProcessing.java
+++ src/com/design/communicate/VoiceProcessing.java
@@ -1,13 +1,11 @@
 package com.design.communicate;
 
 import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
-import com.ibm.watson.developer_cloud.http.HttpMediaType;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+import javax.sound.sampled.AudioSystem;
 
 public class VoiceProcessing {
 	
@@ -15,8 +13,25 @@ public class VoiceProcessing {
 		SpeechToText service = new SpeechToText();
 		service.setUsernameAndPassword("6ce30912-e2be-4a99-9e7a-4e712c5d0cd5", "6XZo4JOLKy6W");
 
-
 		SpeechResults transcript = service.recognize(audio, HttpMediaType.AUDIO_WAV);
+		System.out.println(transcript);
+		return transcript.toString();
+	}
+	
+	public static String processAudio(URL audioURL)
+	{
+		// Set up
+		SpeechToText service = new SpeechToText();
+		service.setUsernameAndPassword("6ce30912-e2be-4a99-9e7a-4e712c5d0cd5", "6XZo4JOLKy6W");
+		
+		// Stream audio
+		InputStream audioIn = new AudioSystem.getAudioInputStream(audioURL);
+		
+		// Transcribe the audio
+		RecognizeOptions options = new RecognizeOptions().continuous(true).interimResults(false	);
+		
+		SpeechResults transcript = service.recognizeWS(audioIn, options, new BaseRecognizeDelegate());
+		
 		System.out.println(transcript);
 		return transcript.toString();
 	}
