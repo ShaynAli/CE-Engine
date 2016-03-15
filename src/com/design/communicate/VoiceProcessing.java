72f1080ecbe7b6c5f821f66ec394e2f466e8748a
Dane Sisinni <dfsisinni@gmail.com>
2016-03-15 03:02:17 -0400

    narrowband

--- src/com/design/communicate/VoiceProcessing.java
+++ src/com/design/communicate/VoiceProcessing.java
@@ -1,10 +1,19 @@
 package com.design.communicate;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.List;
+
+import com.ibm.watson.developer_cloud.http.HttpMediaType;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 
 public class VoiceProcessing {
 	
@@ -20,9 +29,12 @@ public class VoiceProcessing {
 	public static String processAudio(URL audioURL)
 	{
 		SpeechToText service = new SpeechToText();
+		service.setUsernameAndPassword("6ce30912-e2be-4a99-9e7a-4e712c5d0cd5", "6XZo4JOLKy6W");
+		
 		InputStream audioIn = null;
 		try {
 			audioIn = audioURL.openStream();
+			System.out.println(audioIn.toString());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -31,13 +43,24 @@ public class VoiceProcessing {
 		File audio = null;
 		try {
 			audio = File.createTempFile(audioURL.getFile(), ".wav");
+			OutputStream stream = new FileOutputStream(audio);
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			while ((read = audioIn.read(bytes)) != -1) {
+				stream.write(bytes, 0, read);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		audio.deleteOnExit();
 		
-		SpeechResults transcript = service.recognize(audio, HttpMediaType.AUDIO_WAV);
+		RecognizeOptions op = new RecognizeOptions();
+		op.contentType("audio/wav");
+		op.model("en-US_NarrowbandModel");
+		
+		SpeechResults transcript = service.recognize(audio, op);
+		audio.deleteOnExit();
 		return transcript.toString();
 
 	}
